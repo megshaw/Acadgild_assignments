7400982e521c631f6ac30f1230bb7d9a11584809
namsys <namsys@namsys-PC>
2017-03-04 16:10:57 +0530

    Assignment1

---
+++ Assignment1/src/assignment1/Assignment1.java
@@ -0,0 +1,17 @@
+/*
+ * program to print name without using semicolon in the program.
+ */
+package assignment1;
+
+/**
+ *
+ * @author meghana
+ */
+public class Assignment1{
+
+    public static void main(String[] args) {
+        if (System.out.printf("Meghana\n") == null) {}
+
+        if (System.out.append("Meghana\n").equals(null)) {}
+    }
+}
